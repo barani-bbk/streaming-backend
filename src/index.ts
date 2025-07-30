88d2c90c311e3662032540ea649c9fb25722a09c
barani-bbk <bharani.bbk.dev@gmail.com>
2025-07-30 14:15:55 +0530

    add socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (_, res) => {
-  res.send("Hello, Node.js + TypeScript!");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-app.listen(PORT, () => {
+app.use(cors());
+app.use(express.json());
+
+const peers = new Map<string, { socket: Socket }>();
+
+io.on("connection", (socket) => {
+  console.log("🧠 New client connected:", socket.id);
+
+  peers.set(socket.id, {
+    socket,
+  });
+
+  console.log("🧠 Connected peers:", peers.size);
+
+  console.log("Peers:", peers);
+
+  socket.on("disconnect", () => {
+    console.log("❌ Disconnected:", socket.id);
+
+    peers.delete(socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
